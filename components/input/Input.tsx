54be92929a2963d393d3df67545a195e7eee49a3
caarlosdamian <dapecarlos@gmail.com>
2023-11-11 13:03:07 -0600

    feat(componens): added search bar desing

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -4,18 +4,30 @@ import React, { InputHTMLAttributes } from 'react';
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   icon?: string;
   alt?: string;
+  width?: number;
+  height?: number;
+  className?: string;
 }
 
 type UpdatedProps = Props & {
   alt?: Props['icon'] extends undefined ? string : string;
 };
 
-export const Input = ({ icon, alt, ...props }: UpdatedProps) => {
+export const Input = ({
+  icon,
+  alt,
+  height,
+  width,
+  className,
+  ...props
+}: UpdatedProps) => {
   return (
-    <div className="">
-      {icon && <Image src={icon} alt={alt as string} width={24} height={24} />}
+    <div className="flex items-center gap-4">
+      {icon && (
+        <Image src={icon} alt={alt as string} width={width} height={height} />
+      )}
       <input
-        className="text-veryDarkBlue_white  outline-none border-none background-white_veryDarkBlue placeholder:opacity-50"
+        className={`text-veryDarkBlue_white outline-none border-none background-white_veryDarkBlue placeholder:opacity-50 ${className}`}
         {...props}
       />
     </div>
--- components/search/desktopSearch/DesktopSearch.tsx
+++ components/search/desktopSearch/DesktopSearch.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { Input } from '@/components/input/Input';
+import { Button } from '@/components/shared/button/Button';
+import React from 'react';
 
 export const DesktopSearch = () => {
   return (
-    <div>DesktopSearch</div>
-  )
-}
+    <div
+      className="flex items-center h-20 gap-5 
+    pl-6 pr-6 background-white_veryDarkBlue 
+    rounded-md absolute right-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2 
+    min-w-[689px] lg:min-w-[1110px] lg:gap-8"
+    >
+      <Input
+        className="max-w-[145px] lg:max-w-full lg:w-[308px]"
+        placeholder="Filter by title..."
+        icon="/assets/desktop/icon-search.svg"
+        alt="search"
+        height={24}
+        width={24}
+      />
+      <div className="w-[1px] bg-secondary-darkGrey opacity-30 h-full"></div>
+      <Input
+        className="max-w-[145px] lg:max-w-full lg:w-[280px]"
+        placeholder="Filter by location..."
+        icon="/assets/desktop/icon-location.svg"
+        alt="location"
+        width={16}
+        height={24}
+      />
+      <div className="w-[1px] bg-secondary-darkGrey opacity-30 h-full"></div>
+      <div className="flex items-center gap-7 w-full justify-between">
+        <div className="flex items-center gap-4 cursor-pointer">
+          <div className="w-6 h-6 bg-secondary-lightGrey"></div>
+          <span className="text-veryDarkBlue_white paragraph-semibold lg:hidden">
+            Full Time
+          </span>
+          <span className="text-veryDarkBlue_white paragraph-semibold hidden lg:flex">
+            Full Time Only
+          </span>
+        </div>
+        <Button
+          label="Search"
+          className="btn px-[14px] py-4 text-white lg:px-9"
+        />
+      </div>
+    </div>
+  );
+};
